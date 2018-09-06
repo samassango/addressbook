fd3322400058b398963f8c69c260be9f4074689a
Sibusiso Massango <samassango@gmail.com>
2018-09-06 22:05:35 +0200

    Adding the list and the text view and also the save button

--- addressbook/src/App.js
+++ addressbook/src/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'mdbreact/dist/css/mdb.css';
+import Menu from './component/menu';
+import Book from './component/book';
+import Add from './component/add';
+import { Autocomplete, Row, Col, Input, Button, Modal, ModalHeader, ModalBody, Fa } from 'mdbreact';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Menu />
+        <Book />
+        <Input label="Enter new address" icon="lock" iconClass="dark-grey" />
+        <Button color="primary">Save</Button>
       </div>
     );
   }
---
+++ addressbook/src/component/add/index.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react';
+import { Autocomplete, Col } from 'mdbreact';
+
+const states = [
+  "Alabama",
+  "Alaska",
+  "Arizona",
+  "Arkansas",
+  "California",
+  "Colorado",
+  "Connecticut",
+  "Delaware",
+  "Florida",
+  "Georgia",
+  "Hawaii",
+  "Idaho",
+  "Illnois",
+  "Indiana",
+  "Iowa",
+  "Kansas",
+  "Kentucky",
+  "Louisiana",
+  "Maine",
+  "Maryland",
+  "Massachusetts",
+  "Michigan",
+  "Minnesota",
+  "Mississippi",
+  "Missouri",
+  "Montana",
+  "Nebraska",
+  "Nevada",
+  "New Hampshire",
+  "New Jersey",
+  "New Mexico",
+  "New York",
+  "North Carolina",
+  "North Dakota",
+  "Ohio",
+  "Oklahoma",
+  "Oregon",
+  "Pennsylvania",
+  "Rhode Island",
+  "South Carolina",
+  "South Dakota",
+  "Tennessee",
+  "Texas",
+  "Utah",
+  "Vermont",
+  "Virginia",
+  "Washington",
+  "West Virginia",
+  "Wisconsin",
+  "Wyoming"
+];
+
+const MyAutocomplete = (props) => {
+  return (
+    <Col md="6">
+      <Autocomplete
+        data={states}
+        label="What is your favorite US state?"
+        clear
+        clearClass="grey-text" id="input"
+      />
+    </Col>
+  );
+};
+
+export default MyAutocomplete;
